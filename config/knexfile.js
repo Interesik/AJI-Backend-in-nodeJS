6f9f1fb05ed5979be839c2a92b8ea51cacaf5f33
Interesik <interesik.pro@gmail.com>
2023-01-10 00:01:45 +0100

    first commit

---
+++ config/knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+    client: 'postgres',
+    connection: {
+        user: 'postgres',
+        address: '127.0.0.1',
+        port: 5432,
+        password: 'example',
+        database: 'products',
+    }
+}
